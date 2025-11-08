1fe829ffdcb4b74b94b28dd3ba06989ff4621acd
Victor-Pizzaia <v_pizzaia@hotmail.com>
2025-11-08 17:52:54 -0300

    add java project

---
+++ src/main/java/com/christmas/dessert/voting/christmas_dessert_voting/ChristmasDessertVotingApplication.java
@@ -0,0 +1,13 @@
+package com.christmas.dessert.voting.christmas_dessert_voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasDessertVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasDessertVotingApplication.class, args);
+	}
+
+}
